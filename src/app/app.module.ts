df056560f877f57ebc6007a35e5311ece30203d2
Aliaksandr Dounar <dovnar.am@gmail.com>
2017-02-20 13:14:57 +0300

    initial commit
    Signed-off-by: Aliaksandr Dounar <dovnar.am@gmail.com>

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Authentication } from '../pages/authentication/authentication';
+import { Page2 } from '../pages/page2/page2';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Authentication,
+    Page2
+  ],
+  imports: [
+    FormsModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Authentication,
+    Page2
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
