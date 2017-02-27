17cd6fd18a9a61fcb7c301d363318123f76d1296
Ivan Stanevich <i.stanevich@adpro.by>
2017-02-27 14:59:17 +0300

    place choose refactored

--- src/pages/place-choose/place-choose.ts
+++ src/pages/place-choose/place-choose.ts
@@ -1,7 +1,9 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
-import { Geolocation } from 'ionic-native';
+import {Geolocation} from 'ionic-native';
 import {Place} from "../../app/models/place.model";
+import {DashboardPage} from "../dashboard/dashboard";
+import {PlacePage} from "../place/place";
 
 @Component({
   selector: 'page-place-choose',
@@ -16,7 +18,12 @@ export class PlaceChoosePage {
   counter: number = 1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.places.push(new Place({}));
+    // this.places.push(new Place({}));
+
+    for (let i = 0; i < 10; i++) {
+      this.addNewPlace();
+      this.counter++;
+    }
   }
 
   /**
@@ -29,8 +36,17 @@ export class PlaceChoosePage {
    * check tab on type
    * @return {boolean}
    */
-  isList() : boolean {
+  isList(): boolean {
     return this.segment === 'list';
+  }
+
+
+  goToDashboard() {
+    this.navCtrl.push(DashboardPage);
+  }
+
+  goToPlace(place : Place) {
+    this.navCtrl.push(PlacePage , {place : place});
   }
 
   /**
@@ -38,7 +54,14 @@ export class PlaceChoosePage {
    */
   addNewPlace() {
     let newPlaces = Array.from(this.places);
-    newPlaces.push(new Place({latitude: 55+ this.counter, longitude: 32+ this.counter}));
+    newPlaces.push(new Place({name: 'letMeSport test', latitude: 55 + this.counter, longitude: 32 + this.counter}));
     this.places = newPlaces;
   }
+
+  // addNewPlace(place : Place) {
+  //   let newPlaces = Array.from(this.places);
+  //   newPlaces.push(new Place({name: 'letMeSport test' , latitude: 55+ this.counter, longitude: 32+ this.counter}));
+  //   this.places = newPlaces;
+  //   console.log(this.places);
+  // }
 }
