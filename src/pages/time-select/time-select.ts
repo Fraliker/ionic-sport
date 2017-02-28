1f76d8f5db7932ee086bf1bd1d094cbf5bb14424
Ivan Stanevich <i.stanevich@adpro.by>
2017-02-28 18:03:54 +0300

    Russian days names

--- src/pages/time-select/time-select.ts
+++ src/pages/time-select/time-select.ts
@@ -4,12 +4,7 @@ import {MainSportChoosePage} from "../main-sport-choose/main-sport-choose";
 import {PlaceChoosePage} from "../place-choose/place-choose";
 import * as moment  from 'moment';
 import {DashboardPage} from "../dashboard/dashboard";
-/*
-  Generated class for the TimeSelect page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-time-select',
   templateUrl: 'time-select.html'
