0ed42e0f21d3c305f0c9d0061f5178791ca8da89
Ivan Stanevich <i.stanevich@adpro.by>
2017-02-28 14:42:34 +0300

    dashboard
    without data

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {BookInfoPage} from '../pages';
+import {User} from "../../app/models/user.model";
+import {AuthService} from "../../app/services/auth.service";
 /*
  Generated class for the Dashboard page.
 
@@ -13,27 +15,33 @@ import {BookInfoPage} from '../pages';
 })
 export class DashboardPage implements OnInit {
 
-  private booked = ['Jane', 'Stan', 'Genry', 'Margaret'];
-  place : string = 'Корт с грунтовым покрытием';
+  private booked; //= ['Jane', 'Stan', 'Genry', 'Margaret'];
+  place: string = 'Корт с грунтовым покрытием';
   date: string = '19 января 2017 в 18:00';
   price: number = 9900;
+  user: User = new User({});
 
   ngOnInit(): void {
 
   }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public auth: AuthService) {
+
+    this.auth.isAuthentificated().then((res: User) => {
+      this.user = res;
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DashboardPage');
   }
 
   goToBookInfo(book) {
     this.navCtrl.push(BookInfoPage, {'book': book});
   }
 
-  goToDashboard(){
+  goToDashboard() {
 
   }
 }
