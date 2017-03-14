cff7fe7b8e129f9ad16b86873f6a7778828dbfeb
Ivan Stanevich <i.stanevich@adpro.by>
2017-03-14 19:39:48 +0300

    =images bugfix

--- src/models/place.model.ts
+++ src/models/place.model.ts
@@ -19,7 +19,7 @@ export class Place {
 
     this.id = obj.id || null;
     this.image = 'http://2.wlimg.com/product_images/bc-full/dir_21/601811/tennis-court-construction-1705242_service_image.jpg';
-    this.imageLogo = 'https://thumb9.shutterstock.com/display_pic_with_logo/1453121/299886746/stock-vector-vector-illustration-for-logo-for-lawn-tennis-consisting-of-ground-orange-tennis-court-net-racket-299886746.jpg';
+    this.imageLogo = obj.logoSrc || 'https://thumb9.shutterstock.com/display_pic_with_logo/1453121/299886746/stock-vector-vector-illustration-for-logo-for-lawn-tennis-consisting-of-ground-orange-tennis-court-net-racket-299886746.jpg';
     this.type = obj.type || 'Спортивный клуб';
     this.name = obj.name || 'Балашиха';
     this.address = obj.address || 'Не указан';
@@ -37,5 +37,17 @@ export class Place {
       obj.services.forEach((item) => {
         this.services.push(new PlaceService(item))
       });
+
+    if (obj.images != null) {
+      if (obj.images.length != 0) {
+        for (let i = 0; i < obj.images.length; i++) {
+          if (obj.images[i].src != null) {
+            this.image = obj.images[i].src;
+            break;
+          }
+        }
+      }
+    }
+    console.log(obj);
   }
 }
