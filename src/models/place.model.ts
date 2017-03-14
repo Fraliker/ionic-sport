ffcf9f682662e249bf68dca566a16f17f1c359c6
Ivan Stanevich <i.stanevich@adpro.by>
2017-03-14 17:26:45 +0300

    =image

--- src/models/place.model.ts
+++ src/models/place.model.ts
@@ -18,10 +18,8 @@ export class Place {
   constructor(obj) {
 
     this.id = obj.id || null;
-    this.image = (obj.image != null) ? obj.image.origin
-      : 'http://2.wlimg.com/product_images/bc-full/dir_21/601811/tennis-court-construction-1705242_service_image.jpg';
-    this.imageLogo = (obj.logo != null) ? obj.logo.origin
-      : 'https://thumb9.shutterstock.com/display_pic_with_logo/1453121/299886746/stock-vector-vector-illustration-for-logo-for-lawn-tennis-consisting-of-ground-orange-tennis-court-net-racket-299886746.jpg';
+    this.image = 'http://2.wlimg.com/product_images/bc-full/dir_21/601811/tennis-court-construction-1705242_service_image.jpg';
+    this.imageLogo = 'https://thumb9.shutterstock.com/display_pic_with_logo/1453121/299886746/stock-vector-vector-illustration-for-logo-for-lawn-tennis-consisting-of-ground-orange-tennis-court-net-racket-299886746.jpg';
     this.type = obj.type || 'Спортивный клуб';
     this.name = obj.name || 'Балашиха';
     this.address = obj.address || 'Не указан';
