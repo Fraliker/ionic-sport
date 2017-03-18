059f81da3ba48fa82fb3b48bd9953c55902ce7af
Ivan Stanevich <i.stanevich@adpro.by>
2017-03-18 17:10:11 +0300

    config permissions changed

--- src/config/prod.config.ts
+++ src/config/prod.config.ts
@@ -1,3 +1,4 @@
 export default {
-  'API_PATH' : 'http://letmesport.dev.rgt.by/api/'
+  'API_PATH' : 'http://letmesport.dev.rgt.by/api/',
+  'API_ERROR' : 'http://social-event-api.herokuapp.com/error'
 }
---
+++ src/providers/errorHandle.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by ivan on 18.03.17.
+ */
